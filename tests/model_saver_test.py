b39e168cf5830a6befbe8e3f2cf6cc7393ef12f0
ITPROSTA <81316740+itprosta@users.noreply.github.com>
2024-04-25 10:45:53 +0200

    Added saver/loader

--- tests/model_saver_test.py
+++ tests/model_saver_test.py
@@ -1,3 +1,17 @@
 import NeuroWeave
-from NeuroWeave import save_model, load_model
+
+import numpy as np
+
+
+# Test model setup
+test_model = {
+    'weights': [np.array([[0.1, 0.2], [0.3, 0.4]]), np.array([0.5, 0.6])],  # Example weight arrays
+    'config': {
+        'learning_rate': 0.01,
+        'activation': 'relu'
+    }
+}
+
+NeuroWeave.save_model(test_model, 'my_model.h5', overwrite=True)
+
 
