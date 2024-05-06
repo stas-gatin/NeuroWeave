7c38ba4a150e6bcd8acda066c643cb247c5d1343
stas-gatin <stasgatin@gmail.com>
2024-05-06 17:06:39 +0200

    Updated module neuro_storage (loader and saver files)

--- weave/neuro_storage/loader.py
+++ weave/neuro_storage/loader.py
@@ -1,4 +1,8 @@
 import h5py
+import logging
+
+# Configuring logging (if you choose to log events within this function)
+logging.basicConfig(level=logging.INFO)
 
 
 def load_model(file_path):
@@ -11,13 +15,37 @@ def load_model(file_path):
     Returns:
     A dictionary with 'weights' and 'config'.
     """
+    # Initialize an empty model dictionary
     model = {'weights': [], 'config': {}}
-    with h5py.File(file_path, 'r') as file:
-        weights_group = file['weights']
-        model['weights'] = [weights_group[name][()] for name in weights_group]
 
-        config_group = file['config']
-        model['config'] = {key: config_group.attrs[key] for key in
-                           config_group.attrs}
+    if file_path is None or not file_path.strip():
+        logging.error("No file path provided.")
+        raise ValueError("File path is required to save the model. Set file_path='path/to/save'.")
+
+    try:
+        # Attempt to open the specified HDF5 file
+        with h5py.File(file_path, 'r') as file:
+            logging.info(f"Opening the file {file_path} for reading model data.")
+
+            # Load the weights from the file
+            weights_group = file['weights']
+            model['weights'] = [weights_group[name][()] for name in weights_group]
+            logging.info("Weights have been loaded successfully.")
+
+            # Load the configuration from the file
+            config_group = file['config']
+            model['config'] = {key: config_group.attrs[key] for key in config_group.attrs}
+            logging.info("Configuration has been loaded successfully.")
+
+    except OSError as e:
+        logging.error(f"Failed to read the file {file_path}: {e}")
+        raise IOError(f"Cannot open or read the file: {file_path}. Error: {e}")
+
+    except KeyError as e:
+        logging.error(f"Key error in loading data from the file {file_path}: {e}")
+        raise KeyError(f"Problem with data format in the file: {file_path}. Missing key: {e}")
+
+    # Return the loaded model dictionary
+    logging.info(f"Model loaded successfully from {file_path}")
 
     return model
