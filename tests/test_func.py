062dd1868529ee0cb252510a42e4ca945baede7b
stas-gatin <stasgatin@gmail.com>
2024-05-08 13:04:48 +0200

    added functions to functions.py file

--- tests/test_func.py
+++ tests/test_func.py
@@ -3,5 +3,4 @@ from weave import neuro_functions
 import weave
 
 
-zeros_tensor = weave.zeros((3,4), dtype='int')
-print(zeros_tensor)
+array=weave.arange
--- weave/neuro_functions/functions.py
+++ weave/neuro_functions/functions.py
@@ -1,33 +1,67 @@
 import numpy as np
+
 from weave import Tensor
 
 __all__ = [
+    'ones',
+    'ones_like',
+    'empty',
     'zeros',
+    'zeros_like',
+    'full',
+    'full_like',
+    'rand',
+    'eye',
+    'linspace',
+    'arange'
 ]
 
 
-def ones(shape, dtype: str = None):
+def ones(shape, dtype: str = None,
+         use_grad: bool = False, device: str = 'cpu'):
     array = np.ones(shape, dtype=dtype)
-    return Tensor(data=array, dtype=dtype)
+    return Tensor(data=array, dtype=dtype, use_grad=use_grad)
 
 
-def empty(shape, dtype: str = None):
+def ones_like(tensor, dtype: str = None, shape=None,
+         use_grad: bool = False, device: str = 'cpu'):
+    array = np.ones_like(a=tensor.data, dtype=dtype, shape=shape)
+    return Tensor(data=array, dtype=dtype, use_grad=use_grad)
+
+
+def empty(shape, dtype: str = None,
+          use_grad: bool = False, device: str = 'cpu'):
     array = np.empty(shape, dtype=dtype)
-    return Tensor(data=array, dtype=dtype)
+    return Tensor(data=array, dtype=dtype, use_grad=use_grad)
 
 
-
-def full(shape: int = None, fill_value=None, dtype: str = np.inf):
-    array = np.full(shape, dtype=dtype)
-    return Tensor(data=array, dtype=dtype)
-
-
-def rand(shape, dtype=None, use_grad: bool = False, device:str = 'cpu'):
-    array = np.random.rand(shape, dtype)
-    return Tensor(data=array, dtype=dtype)
+def full(shape, fill_value=np.inf, dtype=None,
+         use_grad: bool = False, device: str = 'cpu'):
+    array = np.full(shape, fill_value=fill_value, dtype=dtype)
+    return Tensor(data=array, dtype=dtype, use_grad=use_grad)
 
 
-def zeros(shape, dtype: str = None, use_grad: bool = False, device:str = 'cpu'):
+def full_like(tensor, fill_value=np.inf, dtype: str = None, shape=None,
+         use_grad: bool = False, device: str = 'cpu'):
+    array = np.full_like(a=tensor.data, fill_value=fill_value, dtype=dtype, shape=shape)
+    return Tensor(data=array, dtype=dtype, use_grad=use_grad)
+
+
+def zeros(shape, dtype: str = None,
+          use_grad: bool = False, device: str = 'cpu'):
     array = np.zeros(shape, dtype=dtype)
-    return Tensor(data=array, dtype=dtype)
+    return Tensor(data=array, dtype=dtype, use_grad=use_grad)
+
+
+def zeros_like(tensor, dtype: str = None, shape=None,
+         use_grad: bool = False, device: str = 'cpu'):
+    array = np.zeros_like(a=tensor.data, dtype=dtype, shape=shape)
+    return Tensor(data=array, dtype=dtype, use_grad=use_grad)
+
+
+def rand(*shape,
+         use_grad: bool = False, device: str = 'cpu'):
+    array = np.random.rand(*shape)
+    return Tensor(data=array, dtype=array.dtype, use_grad=use_grad)
+
 
