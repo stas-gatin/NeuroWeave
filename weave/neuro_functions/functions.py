f59a6c7173a04add6e5833095184e36627f56102
stas-gatin <stasgatin@gmail.com>
2024-05-08 11:25:06 +0200

    added functions.py file

--- weave/neuro_functions/functions.py
+++ weave/neuro_functions/functions.py
@@ -1,3 +1,12 @@
 import numpy as np
+from weave import Tensor
 
+
+def ones():
+    pass
+
+
+def zeros(shape, dtype=float, use_grad: bool = False, device:str = 'cpu'):
+    array = np.zeros(shape, dtype=dtype)
+    return Tensor(data=array, dtype=dtype)
 
