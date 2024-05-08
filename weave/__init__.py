a642fc4d0000bb774cc21218773b3aa7c0f81495
stas-gatin <stasgatin@gmail.com>
2024-05-08 11:17:42 +0200

    added functions.py file

--- weave/__init__.py
+++ weave/__init__.py
@@ -3,5 +3,6 @@ from .neuro_storage.saver import save_model
 from .neuro_storage.loader import load_model
 from .neuro_dataset.main import *
 from weave.tensor import Tensor
+from .neuro_functions.functions import *
 
 __version__ = '1.0.0'
