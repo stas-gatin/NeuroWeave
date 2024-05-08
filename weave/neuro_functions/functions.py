9dde0baa1cfe6c0629a5f57b7c0c6a6d82744809
stas-gatin <stasgatin@gmail.com>
2024-05-08 11:37:55 +0200

    added functions to functions.py file

--- weave/neuro_functions/functions.py
+++ weave/neuro_functions/functions.py
@@ -5,11 +5,25 @@ __all__ = [
     'zeros',
 ]
 
-def ones():
-    pass
+
+def ones(shape, dtype: str = None):
+    array = np.ones(shape, dtype=dtype)
+    return Tensor(data=array, dtype=dtype)
 
 
-def zeros(shape, dtype=float, use_grad: bool = False, device:str = 'cpu'):
+def empty(shape, dtype: str = None):
+    array = np.empty(shape, dtype=dtype)
+    return Tensor(data=array, dtype=dtype)
+
+
+def full(shape, fill_value=None, dtype: str = np.inf):
+    array = np.full(shape, dtype=dtype)
+    return Tensor(data=array, dtype=dtype)
+
+
+
+
+def zeros(shape, dtype=None, use_grad: bool = False, device:str = 'cpu'):
     array = np.zeros(shape, dtype=dtype)
     return Tensor(data=array, dtype=dtype)
 
