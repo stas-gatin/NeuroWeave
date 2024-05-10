240e48e515b1346cec2ee24784ac27b1aa7cf326
Shillianne <vshiwork@gmail.com>
2024-05-10 12:52:42 +0200

    Added an environment variable to the __init__ file to allow communication with CUDA.

--- weave/__init__.py
+++ weave/__init__.py
@@ -1,4 +1,6 @@
 # weave/__init__.py
+import os
+os.environ['CUDA_PATH'] = 'C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.4/'
 from .neuro_storage.saver import save_model
 from .neuro_storage.loader import load_model
 from weave.tensor import Tensor
