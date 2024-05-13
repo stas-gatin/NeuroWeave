729906c866ebf8c4d641fb4c870b71c6791d8083
stas-gatin <stasgatin@gmail.com>
2024-05-13 11:46:03 +0200

    Added example file

---
+++ examples/prepare_dataset.py
@@ -0,0 +1,19 @@
+import weave
+from weave import StandardScaler
+
+my_data = weave.Dataset(path="teleCust1000t.csv", file_type='csv')
+
+# -- StandardScaler --
+#scaler = StandardScaler()
+#norm = scaler.fit(my_data.data)
+#scaled_data = scaler.transform(my_data.data)
+#print(scaled_data)
+
+# -- StandardScaler -- otro metodo
+scaled_data2 = StandardScaler().fit_transform(my_data.data)
+print(scaled_data2)
+
+
+# train_test_split
+X_train, X_test, y_train, y_test = my_data.train_test_split(data=scaled_data2, x=['region', 'age'], y='custcat', test_size=0.2, seed=10)
+
