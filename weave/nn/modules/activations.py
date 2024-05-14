Finished Softmax activation function and fixed and added methods in the Tensor class. Some errors in functions.py are due to be fixed.

weave/nn/modules/activations.py
@@ -1,6 +1,6 @@
 import math
 import numpy as np
-from weave import Tensor
+from weave import Tensor, diag, outer, vstack
 from model import Model
 
 
@@ -166,14 +166,32 @@ class Gaussian(Model):
 
 
 class Softmax(Model):
-    def __init__(self):
+    def __init__(self, dim: int = None):
         super().__init__()
+        self.dim = 1 if dim is None else dim
 
     def forward(self, x: Tensor) -> Tensor:
-        pass
+        out = Tensor(data=(x.exp() / x.exp().sum(axis=self.dim).unsqueeze().T), _children=(x,), _op='softmax',
+                     use_grad=x.grad_enabled, device=self.device)
+
+        def _backward():
+            out._grad_enabled = False
+            if len(out.shape) > 1:
+                derivative = []
+                for i in range(out.shape[0]):
+                    d = out.grad @ (diag(out[i].squeeze()) - outer(out[i], out[i]))
+                    derivative.append(d[i])
+                x.grad += vstack(tuple(derivative))
+            else:
+                x.grad += out.grad @ (diag(out.squeeze()) - outer(out, out))
+            out._grad_enabled = True
+
+        if x.grad_enabled:
+            out._backward = _backward
+        return out
 
     def __call__(self, x: Tensor) -> Tensor:
-        pass
+        return self.forward(x)
 
     def __repr__(self) -> str:
-        pass
+        return 'Softmax()'

weave/tensor.py
@@ -638,7 +638,7 @@ class Tensor(np.ndarray):
     def round(self, decimals=0, out=None):
         return Tensor(data=self.data.round(decimals, out), use_grad=self._grad_enabled, device=self.device)
 
-    def squeeze(self, axis=0):
+    def squeeze(self, axis=0) -> "Tensor":
         try:
             out = Tensor(data=self.data.squeeze(axis), use_grad=self._grad_enabled, device=self.device)
             if not isinstance(self.grad, int):
@@ -647,7 +647,7 @@ class Tensor(np.ndarray):
             out = self
         return out
 
-    def unsqueeze(self, axis=0):
+    def unsqueeze(self, axis=0) -> "Tensor":
         val = np.expand_dims(self.data, axis=axis) if isinstance(self.data, np.ndarray) else \
               cp.expand_dims(self.data, axis=axis)
         out = Tensor(data=val, use_grad=self._grad_enabled, device=self.device)
@@ -661,7 +661,8 @@ class Tensor(np.ndarray):
         return self.data.std(axis=axis, dtype=dtype, out=out, ddof=ddof, keepdims=keepdims)
 
     def sum(self, axis=None, dtype=None, out=None, keepdims=False, initial=None, where=True):
-        return self.data.sum(axis=axis, dtype=dtype, out=out, keepdims=keepdims)
+        val = self.data.sum(axis=axis, dtype=dtype, out=out, keepdims=keepdims)
+        return Tensor(data=val, use_grad=self._grad_enabled, device=self.device)
 
     def take(self, indices, axis=None, out=None, mode='raise'):
         return Tensor(data=self.data.take(indices, axis=axis, out=out), use_grad=self._grad_enabled,