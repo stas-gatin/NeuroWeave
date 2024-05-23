9d95a4c38ac23c4c65a5d6bc1933527f435377f8
stas-gatin <stasgatin@gmail.com>
2024-05-23 21:56:36 +0200

    Minor updates and refactorings.

--- examples/prepare_dataset.py
+++ examples/prepare_dataset.py
@@ -1,5 +1,5 @@
 import weave
-from weave import StandardScaler
+from weave import StandardScaler, train_test_split
 
 my_data = weave.Dataset(path="teleCust1000t.csv", file_type='csv')
 
@@ -15,5 +15,5 @@ print(scaled_data2)
 
 
 # train_test_split
-X_train, X_test, y_train, y_test = my_data.train_test_split(data=scaled_data2, x=['region', 'age'], y='custcat', test_size=0.2, seed=10)
+X_train, X_test, y_train, y_test = train_test_split(data=scaled_data2, x=['region', 'age'], y='custcat', test_size=0.2, seed=10)
 
